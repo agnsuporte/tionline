2ebc4046365d5ae08e5b4f3663197cd497f660d8
Alexandre Gomes <agnsuporte@gmail.com>
2019-06-21 10:54:46 -0300

    Primeiro Commit: v1.0.0 - correcao no firebase

--- src/util/firebaseUtils.js
+++ src/util/firebaseUtils.js
@@ -4,24 +4,14 @@ import 'firebase/firebase-auth';
 import 'firebase/firebase-storage';
 import 'firebase/firebase-database';
 
-// Initialize Firebase
-// let config = {
-//     apiKey: "AIzaSyA4eFi8qSfR6l3POO2-YYuTvYHrwcDE9f4",
-//     authDomain: "agn-suporte-online.firebaseapp.com",
-//     databaseURL: "https://agn-suporte-online.firebaseio.com",
-//     projectId: "agn-suporte-online",
-//     storageBucket: "agn-suporte-online.appspot.com",
-//     messagingSenderId: "390197291168"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBhS25KpoKwxrNuO8U1-3BTbI_1TBULuDk",
-    authDomain: "tionline-dfafc.firebaseapp.com",
-    databaseURL: "https://tionline-dfafc.firebaseio.com",
-    projectId: "tionline-dfafc",
-    storageBucket: "tionline-dfafc.appspot.com",
-    messagingSenderId: "3467808054",
-    appId: "1:3467808054:web:33d9b133f03ba498"
+    apiKey: "*",
+    authDomain: "*",
+    databaseURL: "*",
+    projectId: "*",
+    storageBucket: "*",
+    messagingSenderId: "*",
+    appId: "*"
 };
 
 const fdb = firebase.initializeApp(firebaseConfig);
